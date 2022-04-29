5b8a8873387c98f631d45f5f4c3243675b8fa9d1
Ivo <warriors123@abv.bg>
2022-04-29 23:30:32 +0300

    new settings file

--- movie_review_project/settings.py
+++ movie_review_project/settings.py
@@ -96,9 +96,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR / 'staticfiles'),)
 
 MEDIA_URL = '/media/'
 
@@ -107,5 +105,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.MovieReviewUser'
-
-django_heroku.settings(locals())
